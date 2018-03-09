9e095f641ab54c29e15c6aab21de3e681b729afd
ClementLiu <ziwei.liu@cca.edu>
2018-03-08 17:15:05 -0800

    add css autoprefix add sample

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,13 +1,18 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('styles', function() {
     gulp.src('sass/**/*.scss')
-        .pipe(sass({outputStyle: 'nested'}).on('error', sass.logError))
+        .pipe(sass({ outputStyle: 'nested' }).on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 3 versions'],
+            cascade: false
+        }))
         .pipe(gulp.dest('./css/'));
 });
 
 //Watch task
-gulp.task('default',function() {
-    gulp.watch('sass/**/*.scss',['styles']);
+gulp.task('default', function() {
+    gulp.watch('sass/**/*.scss', ['styles']);
 });
